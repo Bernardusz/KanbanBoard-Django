4c5efc625a74f6fdf094f97e4149f457b40816ef
Bernardusz <b.christian.nugroho.study@gmail.com>
2025-09-29 04:11:59 +0700

    Finished add page, still need polishing

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -93,7 +93,7 @@ const Column = ({columnData}: {columnData: column}) => {
 }
 
 
-const Board = ({boardData, setBoard}: {boardData: board, setBoard:React.Dispatch<React.SetStateAction<board>>}) => {
+const Board = ({boardData, setBoard, setIsBoard}: {boardData: board, setBoard:React.Dispatch<React.SetStateAction<board>>, setIsBoard:React.Dispatch<React.SetStateAction<boolean>>}) => {
     const [activeTask, setActiveTask] = useState<task | null>(null)
     function parseId(id: string){
         const [type, num] = id.split("-");
@@ -191,12 +191,52 @@ const Board = ({boardData, setBoard}: {boardData: board, setBoard:React.Dispatch
                     </DragOverlay>
                 </DndContext>
             </div>
+            <button className='fixed bottom-4 right-6' onClick={() => setIsBoard}>Create a new Task</button>
+        </div>
+    );
+}
+
+const AddPage = () => {
+    const [title, setTitle] = useState<string>("")
+    const [description, setDescription] = useState<string>("")
+    const [type, setType] = useState<"toDo" | "inProgress" | "inReview" | "done">("toDo")
+    return (
+        <div className="flex flex-col w-full min-h-screen justify-center items-center"> {/* The Page */} 
+            <div className="p-15 rounded-xl flex flex-col items-center justify-center align-middle h-fit w-xl border-2 bg-blue-100 gap-2"> {/* The box in the middle */}
+
+                <div className="p-2 w-full flex flex-col gap-1"> {/* Header */}
+                    <h6 className="font-light text-gray-600 text-left">Hello grinder</h6>
+                    <h3 className="text-2xl font-medium text-gray-600 text-left">WNew Board incoming !</h3>
+                </div> 
+
+                <div className="flex flex-col w-full gap-2"> {/* Login information */}
+                    <div className="flex flex-col p-4">
+                        <div className="flex flex-col w-full h-fit gap-2">
+                            <input className="text-gray-900 w-full h-10 border-1 border-gray-600 pl-2" type="text" value={title} onChange={event => setTitle(event.target.value)} placeholder="Title" />
+                            <input className="text-gray-900 w-full h-10 border-1 border-gray-600 pl-2" type="text" value={description} onChange={event => setDescription(event.target.value)} placeholder="Description" />
+                            <div>    
+                                <input type="radio" name="type" id="todo" onClick={() => setType("toDo")} />
+                                <label htmlFor="todo">ToDo</label>
+                                <input type="radio" name='type' id='in-progress' onClick={() => setType("inProgress")}/>
+                                <label htmlFor="in-progress">In Progress</label>
+                                <input type="radio" name='type' id='in-review' onClick={() => setType("inReview")}/>
+                                <label htmlFor="in-review">In Review</label>
+                                <input type="radio" name='type' id='done' onClick={() => setType("done")}/>
+                                <label htmlFor="done">Done</label>
+                            </div>
+                        </div>
+
+                    </div>
+                    <button className="mt-2 bg-blue-500 p-2 rounded-md" >Login</button>
+                </div>
+            </div>
+            <button className="mt-2 bg-blue-500 p-2 rounded-md">Back</button>
         </div>
     );
 }
 
 const BoardPage = () => {
-    // const {boardId} = useParams();
+    const [isBoard, setIsBoard] = useState<boolean>(true);
     const [board, setBoard] = useState<board>({
     id: 1,
     title: "My Kanban",
@@ -212,10 +252,16 @@ const BoardPage = () => {
     ],
     });
     return (
-        <div className="flex flex-col w-full min-h-screen justify-center items-center"> {/* The Page */} 
-            <Board boardData={board} setBoard={setBoard}/>
+        <div>
+            { isBoard ? (
+                <Board boardData={board} setBoard={setBoard} setIsBoard={() => setIsBoard(false)}/>
+            ) : (
+                <AddPage/>
+            )
+
+            }
         </div>
-    );
+        );
 }
 
 export default BoardPage;
--- frontend/src/pages/List.tsx
+++ frontend/src/pages/List.tsx
@@ -1,5 +1,6 @@
 import trashIcon from '../assets/trash.png'
 import editIcon from '../assets/edit.png'
+import React, { useState } from 'react'
 
 type props = {
     id: number,
@@ -28,19 +29,54 @@ const BoardCard = ({id, title, created_at, description}: props) => {
     );
 }
 
+const AddBoard = ({title, setTitle, description, setDescription}: 
+    {title: string,
+     setTitle: React.Dispatch<React.SetStateAction<string>>,
+     description: string,
+     setDescription: React.Dispatch<React.SetStateAction<string>>
+    }) => {
+    return (
+        <div className="p-15 rounded-xl flex flex-col items-center justify-center align-middle h-fit w-xl border-2 bg-blue-100 gap-2"> {/* The box in the middle */}
+
+                <div className="p-2 w-full flex flex-col gap-1"> {/* Header */}
+                    <h6 className="font-light text-gray-600 text-left">Hello grinder</h6>
+                    <h3 className="text-2xl font-medium text-gray-600 text-left">Welcome back !</h3>
+                </div> 
+
+                <div className="flex flex-col w-full gap-2"> {/* Login information */}
+                    <div className="flex flex-col p-4">
+                        <div className="flex flex-col w-full h-fit gap-2">
+                            <input className="text-gray-900 w-full h-10 border-1 border-gray-600 pl-2" type="text" value={title} placeholder="Title" onChange={event => setTitle(event.target.value)} />
+                            <input className="text-gray-900 w-full h-10 border-1 border-gray-600 pl-2" type="text" value={description} onChange={event => setDescription(event.target.value)} placeholder="Description" />
+                        </div>
+                    </div>
+                    <button className="mt-2 bg-blue-500 p-2 rounded-md" >Login</button>
+                </div>
+            </div>
+    );
+}
+
 const ListPage = () => {
     const myBoards: props[] = [
         {id : 1, title: "MyWorks", created_at: "22-8-2010", description: "My Kanban board for work"},
         {id : 2, title: "Bruh", created_at: "22-9-1020", description: "Just Bruhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"}
     ]
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [isBoard, setIsBoard] = useState(true);
     return (
         <div className="flex flex-col w-full min-h-screen items-center mt-5">
-            <div className="p-15 rounded-xl flex flex-col items-center justify-center align-middle h-fit w-xl border-2 bg-gray-100 gap-2">
-                {myBoards.map(board => (
-                    <BoardCard key={board.id} id={board.id} title={board.title} created_at={board.created_at} description={board.description}/>
-                ))}
-            </div>
-            
+            {isBoard ? (
+                <div className="p-15 rounded-xl flex flex-col items-center justify-center align-middle h-fit w-xl border-2 bg-gray-100 gap-2">
+                    {myBoards.map(board => (
+                        <BoardCard key={board.id} id={board.id} title={board.title} created_at={board.created_at} description={board.description}/>
+                    ))}
+                </div>
+            ): (
+                <AddBoard title={title} description={description}
+                          setDescription={setDescription} setTitle={setTitle}/>
+            )}
+            <button className='fixed bottom-4 right-6' onClick={() => setIsBoard(!isBoard)}></button>
         </div>
     );
 }
